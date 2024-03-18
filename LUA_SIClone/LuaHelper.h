550bdc9b94508c7324a5e559774937cd834d04c1
Albi-back <94167011+Albi-back@users.noreply.github.com>
2024-03-18 14:40:49 +0000

    initial commit adding of files

---
+++ LUA_SIClone/LuaHelper.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <functional>
+#include <map>
+
+extern "C"
+{
+	#include "..\lua-5.4.4\include\lua.h"
+	#include "..\lua-5.4.4\include\lauxlib.h"
+	#include "..\lua-5.4.4\include\lualib.h"
+}
